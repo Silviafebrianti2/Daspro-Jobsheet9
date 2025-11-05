e44bb6f3424885ea0a9e2152099355ca457a405e
Silviafebrianti2 <silviafebrianti517@gmail.com>
2025-11-05 08:21:40 +0700

    percobaan1

--- ArrayBilangan26.java
+++ ArrayBilangan26.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public class ArrayBilangan26 {
 
     public static void main(String[] args ) {
-        Scanner sc = new Scanner(System.in);
         int[] bil = new int[4];
 
         bil[0] = 5;
